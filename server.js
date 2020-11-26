49300774aa096cbf898a605c771b7897ab799335
SedemQuame <sedem.anekpewu.3@gmail.com>
2020-11-26 13:54:41 +0000

    production build application.

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const app = express();
 
 app.use(express.static(__dirname+"/dist/app-ui"));
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/dist/app-ui/index.html'));
+app.all('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/dist'));
 });
 
 var server = app.listen(process.env.PORT || 8080, () => {
     var port = server.address().port;
+    console.log("App started on " + port);
 });
