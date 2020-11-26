39ac76bad56878b84754a914292e4d2102fc33fb
SedemQuame <sedem.anekpewu.3@gmail.com>
2020-11-26 12:24:42 +0000

    add ng.

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname+"/dist/"));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'), );
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
